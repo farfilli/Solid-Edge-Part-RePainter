880ccaa6379edf7b3ab336da3318749c709d02a4
Francesco <francesco.arfilli@gmail.com>
2023-11-21 12:03:40 +0100

    Add project files.

---
+++ PartRepainter.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PartRepainter
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PartRepainter))
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.StylesGrid = New System.Windows.Forms.DataGridView()
+        Me.MenuStyles = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Status = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripDropDownButton1 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.colName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colBodies = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colFeatures = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colFaces = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ToolStripDropDownButton2 = New System.Windows.Forms.ToolStripDropDownButton()
+        CType(Me.StylesGrid, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'StylesGrid
+        '
+        Me.StylesGrid.AllowUserToAddRows = False
+        Me.StylesGrid.AllowUserToDeleteRows = False
+        Me.StylesGrid.AllowUserToResizeColumns = False
+        Me.StylesGrid.AllowUserToResizeRows = False
+        Me.StylesGrid.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.StylesGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
+        Me.StylesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.StylesGrid.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colName, Me.colBodies, Me.colFeatures, Me.colFaces})
+        Me.StylesGrid.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.StylesGrid.Location = New System.Drawing.Point(0, 0)
+        Me.StylesGrid.MultiSelect = False
+        Me.StylesGrid.Name = "StylesGrid"
+        Me.StylesGrid.ReadOnly = True
+        Me.StylesGrid.RowHeadersWidth = 25
+        Me.StylesGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.StylesGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.StylesGrid.Size = New System.Drawing.Size(384, 339)
+        Me.StylesGrid.TabIndex = 0
+        '
+        'MenuStyles
+        '
+        Me.MenuStyles.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MenuStyles.MaximumSize = New System.Drawing.Size(0, 800)
+        Me.MenuStyles.Name = "MenuStyles"
+        Me.MenuStyles.Size = New System.Drawing.Size(61, 4)
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripDropDownButton1, Me.ToolStripDropDownButton2, Me.Status})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 339)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(384, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Status
+        '
+        Me.Status.Name = "Status"
+        Me.Status.Size = New System.Drawing.Size(39, 17)
+        Me.Status.Text = "Ready"
+        '
+        'ToolStripDropDownButton1
+        '
+        Me.ToolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripDropDownButton1.Image = CType(resources.GetObject("ToolStripDropDownButton1.Image"), System.Drawing.Image)
+        Me.ToolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton1.Name = "ToolStripDropDownButton1"
+        Me.ToolStripDropDownButton1.ShowDropDownArrow = False
+        Me.ToolStripDropDownButton1.Size = New System.Drawing.Size(20, 20)
+        Me.ToolStripDropDownButton1.Text = "Refresh"
+        Me.ToolStripDropDownButton1.Visible = False
+        '
+        'colName
+        '
+        Me.colName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.colName.DataPropertyName = "Name"
+        Me.colName.HeaderText = "Name"
+        Me.colName.Name = "colName"
+        Me.colName.ReadOnly = True
+        Me.colName.Resizable = System.Windows.Forms.DataGridViewTriState.[False]
+        '
+        'colBodies
+        '
+        Me.colBodies.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None
+        Me.colBodies.DataPropertyName = "BodiesCount"
+        DataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        Me.colBodies.DefaultCellStyle = DataGridViewCellStyle4
+        Me.colBodies.HeaderText = "Bodies"
+        Me.colBodies.Name = "colBodies"
+        Me.colBodies.ReadOnly = True
+        Me.colBodies.Resizable = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.colBodies.Width = 55
+        '
+        'colFeatures
+        '
+        Me.colFeatures.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None
+        Me.colFeatures.DataPropertyName = "FeaturesCount"
+        DataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        Me.colFeatures.DefaultCellStyle = DataGridViewCellStyle5
+        Me.colFeatures.HeaderText = "Features"
+        Me.colFeatures.Name = "colFeatures"
+        Me.colFeatures.ReadOnly = True
+        Me.colFeatures.Resizable = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.colFeatures.Width = 55
+        '
+        'colFaces
+        '
+        Me.colFaces.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None
+        Me.colFaces.DataPropertyName = "FacesCount"
+        DataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        Me.colFaces.DefaultCellStyle = DataGridViewCellStyle6
+        Me.colFaces.HeaderText = "Faces"
+        Me.colFaces.Name = "colFaces"
+        Me.colFaces.ReadOnly = True
+        Me.colFaces.Resizable = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.colFaces.Width = 55
+        '
+        'ToolStripDropDownButton2
+        '
+        Me.ToolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripDropDownButton2.Image = CType(resources.GetObject("ToolStripDropDownButton2.Image"), System.Drawing.Image)
+        Me.ToolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton2.Name = "ToolStripDropDownButton2"
+        Me.ToolStripDropDownButton2.ShowDropDownArrow = False
+        Me.ToolStripDropDownButton2.Size = New System.Drawing.Size(20, 20)
+        Me.ToolStripDropDownButton2.Text = "Restore"
+        '
+        'PartRepainter
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 361)
+        Me.Controls.Add(Me.StylesGrid)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "PartRepainter"
+        Me.Text = "Solid Edge PartRepainter v0.1"
+        CType(Me.StylesGrid, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StylesGrid As DataGridView
+    Friend WithEvents MenuStyles As ContextMenuStrip
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents Status As ToolStripStatusLabel
+    Friend WithEvents ToolStripDropDownButton1 As ToolStripDropDownButton
+    Friend WithEvents colName As DataGridViewTextBoxColumn
+    Friend WithEvents colBodies As DataGridViewTextBoxColumn
+    Friend WithEvents colFeatures As DataGridViewTextBoxColumn
+    Friend WithEvents colFaces As DataGridViewTextBoxColumn
+    Friend WithEvents ToolStripDropDownButton2 As ToolStripDropDownButton
+End Class
---
+++ PartRepainter.vb
@@ -0,0 +1,410 @@
+﻿Imports SolidEdgeFramework
+Imports System.Reflection
+
+Public Class PartRepainter
+
+    Dim objApp As SolidEdgeFramework.Application
+    Dim objPart As Object
+
+    Dim UsedStyles As New Collection
+
+    Private Sub PartRepainter_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Connect()
+
+        For Each tmpStile As SolidEdgeFramework.FaceStyle In objPart.FaceStyles
+
+            MenuStyles.Items.Add(tmpStile.StyleName, ColorIcon(tmpStile.Opacity, tmpStile.DiffuseRed, tmpStile.DiffuseGreen, tmpStile.DiffuseBlue).ToBitmap)
+
+        Next
+
+        Ricarica()
+
+    End Sub
+
+    Private Sub Connect()
+
+        Try
+            objApp = GetObject(, "SolidEdge.Application")
+        Catch ex As Exception
+            MsgBox("Solid Edge must be running")
+            End
+        End Try
+
+        If objApp.Documents.Count = 0 Then
+            MsgBox("A part or sheetmetal document must be open")
+            End
+        End If
+
+        Select Case objApp.ActiveDocumentType
+            Case Is = SolidEdgeFramework.DocumentTypeConstants.igPartDocument, SolidEdgeFramework.DocumentTypeConstants.igSyncPartDocument, SolidEdgeFramework.DocumentTypeConstants.igSheetMetalDocument, SolidEdgeFramework.DocumentTypeConstants.igSyncSheetMetalDocument
+
+            Case Else
+                MsgBox("A part or sheetmetal document must be open")
+                End
+
+        End Select
+
+        objPart = objApp.ActiveDocument
+
+    End Sub
+
+    Private Sub DeConnect()
+
+        objPart = Nothing
+        objApp = Nothing
+
+    End Sub
+
+    Private Sub Ricarica()
+
+        UsedStyles.Clear()
+
+        Cursor = Cursors.WaitCursor
+        Status.Text = "Retrieving used FaceStyles"
+        Connect()
+
+        Dim Models As SolidEdgePart.Models = objPart.Models
+
+        If Models.Count = 0 Then
+            MsgBox("No design bodies found")
+            End
+        Else
+
+            Dim totFaces As Integer = 0
+            For Each tmpModel As SolidEdgePart.Model In Models
+
+                Try
+                    Dim tmpBody = CType(tmpModel.Body, SolidEdgeGeometry.Body)
+
+                    If Not IsNothing(tmpBody.Style) Then
+                        If Not UsedStyles.Contains(tmpBody.Style.StyleName) Then
+
+                            Dim tmpStile As New Stile
+                            tmpStile.Name = tmpBody.Style.StyleName
+                            tmpStile.Original = tmpBody.Style.StyleName
+                            tmpStile.Bodies.Add(tmpBody)
+                            tmpStile.BodiesCount = 1
+                            UsedStyles.Add(tmpStile, tmpBody.Style.StyleName)
+
+                        Else
+
+                            UsedStyles.Item(tmpBody.Style.StyleName).Bodies.Add(tmpBody)
+                            UsedStyles.Item(tmpBody.Style.StyleName).BodiesCount += 1
+
+                        End If
+
+                    End If
+
+                    Dim FaceType = SolidEdgeGeometry.FeatureTopologyQueryTypeConstants.igQueryAll
+                    Dim tmpFaces = CType(tmpBody.Faces(FaceType), SolidEdgeGeometry.Faces)
+
+                    For Each tmpFace As SolidEdgeGeometry.Face In tmpFaces
+
+                        If Not IsNothing(tmpFace.Style) Then
+                            If Not UsedStyles.Contains(tmpFace.Style.StyleName) Then
+
+                                Dim tmpStile As New Stile
+                                tmpStile.Name = tmpFace.Style.StyleName
+                                tmpStile.Original = tmpFace.Style.StyleName
+                                tmpStile.Faces.Add(tmpFace)
+                                tmpStile.FacesCount = 1
+                                UsedStyles.Add(tmpStile, tmpFace.Style.StyleName)
+
+                            Else
+
+                                UsedStyles.Item(tmpFace.Style.StyleName).Faces.Add(tmpFace)
+                                UsedStyles.Item(tmpFace.Style.StyleName).FacesCount += 1
+
+                            End If
+
+                        End If
+
+                    Next
+
+                Catch ex As Exception
+
+                End Try
+
+            Next
+
+            For Each tmpFeature In objPart.DesignEdgebarFeatures
+
+                Try
+                    Dim tmpStyle As SolidEdgeFramework.FaceStyle = tmpFeature.GetStyle
+                    If Not IsNothing(tmpStyle) Then
+                        If Not UsedStyles.Contains(tmpStyle.StyleName) Then
+
+                            Dim tmpStile As New Stile
+                            tmpStile.Name = tmpStyle.StyleName
+                            tmpStile.Original = tmpStyle.StyleName
+                            tmpStile.Features.Add(tmpFeature)
+                            tmpStile.FeaturesCount = 1
+                            UsedStyles.Add(tmpStile, tmpStyle.StyleName)
+
+                        Else
+
+                            UsedStyles.Item(tmpStyle.StyleName).Features.Add(tmpFeature)
+                            UsedStyles.Item(tmpStyle.StyleName).FeaturesCount += 1
+
+                        End If
+                    End If
+
+                Catch ex As Exception
+
+                End Try
+
+            Next
+
+            StylesGrid.DataSource = UsedStyles
+
+            Try
+                StylesGrid.Refresh()
+            Catch ex As Exception
+            End Try
+
+
+        End If
+
+        DeConnect()
+        Cursor = Cursors.Default
+        Status.Text = "Ready"
+
+    End Sub
+
+    Private Sub StylesGrid_CellMouseClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles StylesGrid.CellMouseClick
+
+        If e.Button = MouseButtons.Right Then
+
+            StylesGrid.CurrentCell = StylesGrid(e.ColumnIndex, e.RowIndex)
+            MenuStyles.Show(StylesGrid, New Point(e.X, e.Y))
+
+        End If
+
+    End Sub
+
+    Private Sub MenuStyles_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MenuStyles.ItemClicked
+
+        Cursor = Cursors.WaitCursor
+        Connect()
+
+        MenuStyles.Hide()
+
+        Dim OldStyleIndex As Integer = StylesGrid.CurrentCell.RowIndex + 1
+        Dim NewStyle As String = e.ClickedItem.Text
+
+        Dim tmpStile As Stile = UsedStyles.Item(OldStyleIndex)
+        Dim counter As Integer = 1
+
+        For Each tmpBody As SolidEdgeGeometry.Body In tmpStile.Bodies
+            Status.Text = "Change style in body " & counter.ToString & "/" & tmpStile.Bodies.Count.ToString
+            tmpBody.Style = objPart.FaceStyles.Item(NewStyle)
+            counter += 1
+        Next
+
+        counter = 1
+
+        For Each tmpFeature As Object In tmpStile.Features
+            Status.Text = "Change style in feature " & counter.ToString & "/" & tmpStile.Features.Count.ToString
+            tmpFeature.SetStyle(objPart.FaceStyles.Item(NewStyle))
+            counter += 1
+        Next
+
+        counter = 1
+
+        For Each tmpFace As SolidEdgeGeometry.Face In tmpStile.Faces
+            Status.Text = "Change style in face " & counter.ToString & "/" & tmpStile.Faces.Count.ToString
+            tmpFace.Style = objPart.FaceStyles.Item(NewStyle)
+            counter += 1
+        Next
+
+        UsedStyles.Item(OldStyleIndex).Name = NewStyle
+        StylesGrid.Refresh()
+
+        Status.Text = "Ready"
+        Cursor = Cursors.Default
+
+    End Sub
+
+    Private Sub ToolStripDropDownButton1_Click(sender As Object, e As EventArgs) Handles ToolStripDropDownButton1.Click
+
+        Ricarica()
+
+    End Sub
+
+    Private Function ColorIcon(A As Double, R As Double, G As Double, B As Double) As Icon
+
+        Dim oIcon As Icon = Nothing
+        Dim area As Integer = 16
+        Try
+
+            Dim bm As New Bitmap(area, area)
+            Dim gph As Graphics = Graphics.FromImage(CType(bm, Image))
+            gph.SmoothingMode = Drawing.Drawing2D.SmoothingMode.AntiAlias
+
+            Dim rec As New Rectangle(0, 0, area - 1, area - 1)
+
+            Dim brushy As Brush
+            brushy = New Drawing.SolidBrush(Color.FromArgb(A * 255, R * 255, G * 255, B * 255))
+
+            gph.FillEllipse(brushy, rec)
+            gph.DrawEllipse(Pens.Black, rec)
+
+            oIcon = Drawing.Icon.FromHandle(bm.GetHicon)
+
+            gph.Dispose()
+            bm.Dispose()
+
+        Catch ex As Exception
+            Debug.WriteLine(ex.ToString)
+        End Try
+
+        Return oIcon
+
+    End Function
+
+    Private Sub ToolStripDropDownButton2_Click(sender As Object, e As EventArgs) Handles ToolStripDropDownButton2.Click
+
+        Cursor = Cursors.WaitCursor
+        Connect()
+
+        For Each tmpStile As Stile In UsedStyles
+
+            If tmpStile.Name <> tmpStile.Original Then
+
+                Dim counter As Integer = 1
+
+                For Each tmpBody As SolidEdgeGeometry.Body In tmpStile.Bodies
+                    Status.Text = "Change style in body " & counter.ToString & "/" & tmpStile.Bodies.Count.ToString
+                    tmpBody.Style = objPart.FaceStyles.Item(tmpStile.Original)
+                    counter += 1
+                Next
+
+                counter = 1
+                For Each tmpFeature As Object In tmpStile.Features
+                    Status.Text = "Change style in feature " & counter.ToString & "/" & tmpStile.Features.Count.ToString
+                    tmpFeature.SetStyle(objPart.FaceStyles.Item(tmpStile.Original))
+                    counter += 1
+                Next
+
+                counter = 1
+
+                For Each tmpFace As SolidEdgeGeometry.Face In tmpStile.Faces
+                    Status.Text = "Change style in face " & counter.ToString & "/" & tmpStile.Faces.Count.ToString
+                    tmpFace.Style = objPart.FaceStyles.Item(tmpStile.Original)
+                    counter += 1
+                Next
+
+                tmpStile.Name = tmpStile.Original
+                StylesGrid.Refresh()
+
+            End If
+
+        Next
+
+        Status.Text = "Ready"
+        Cursor = Cursors.Default
+
+    End Sub
+
+    Private Sub PartRepainter_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+
+        Connect()
+        Dim objHighlightSet As SolidEdgeFramework.HighlightSet
+
+        If objPart.HighlightSets.Count > 0 Then
+
+            objHighlightSet = objPart.HighlightSets.Item(1)
+            objHighlightSet.RemoveAll()
+
+            objHighlightSet.Draw()
+
+        End If
+        DeConnect()
+
+    End Sub
+
+    Private Sub StylesGrid_CellMouseEnter(sender As Object, e As DataGridViewCellEventArgs) Handles StylesGrid.CellMouseEnter
+
+        If e.ColumnIndex = -1 And e.RowIndex <> -1 Then
+
+            Connect()
+
+            Dim StyleIndex As Integer = e.RowIndex + 1
+            Dim tmpStile As Stile = UsedStyles.Item(StyleIndex)
+
+            Dim objHighlightSet As SolidEdgeFramework.HighlightSet
+
+            If objPart.HighlightSets.Count > 0 Then
+
+                objHighlightSet = objPart.HighlightSets.Item(1)
+                objHighlightSet.RemoveAll()
+
+            Else
+
+                objHighlightSet = objPart.HighlightSets.Add()
+
+            End If
+
+            For Each item In tmpStile.Bodies
+                objHighlightSet.AddItem(item)
+            Next
+
+            For Each item In tmpStile.Features
+                objHighlightSet.AddItem(item)
+            Next
+
+            For Each item In tmpStile.Faces
+                objHighlightSet.AddItem(item)
+            Next
+
+            objHighlightSet.Draw()
+
+        Else
+
+            Connect()
+            Dim objHighlightSet As SolidEdgeFramework.HighlightSet
+
+            If objPart.HighlightSets.Count > 0 Then
+
+                objHighlightSet = objPart.HighlightSets.Item(1)
+                objHighlightSet.RemoveAll()
+
+                objHighlightSet.Draw()
+
+            End If
+            DeConnect()
+
+        End If
+
+    End Sub
+
+    Private Sub PartRepainter_LostFocus(sender As Object, e As EventArgs) Handles Me.LostFocus
+
+        Connect()
+        Dim objHighlightSet As SolidEdgeFramework.HighlightSet
+
+        If objPart.HighlightSets.Count > 0 Then
+
+            objHighlightSet = objPart.HighlightSets.Item(1)
+            objHighlightSet.RemoveAll()
+
+            objHighlightSet.Draw()
+
+        End If
+        DeConnect()
+
+    End Sub
+End Class
+
+Public Class Stile
+    Public Property Name As String
+    Public Property Original As String
+    Public Property Bodies As New Collection
+    Public Property BodiesCount As Integer = 0
+    Public Property Features As New Collection
+    Public Property FeaturesCount As Integer = 0
+    Public Property Faces As New Collection
+    Public Property FacesCount As Integer = 0
+
+End Class
